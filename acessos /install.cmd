2e33ad7d7f2f5173916945a4c309148b302f53b2
Giuseph66 <giusephgangareli@gmail.com>
2025-07-04 12:19:31 -0400

    sep

--- acessos /install.cmd
+++ acessos /install.cmd
@@ -14,7 +14,10 @@ where node >nul 2>&1
 if %errorLevel% EQU 0 (
   echo Node.js já está instalado. Versão:
   node -v
-  goto CONTINUAR
+  echo.
+  echo Pronto! Agora execute o script instalar_projeto.cmd para continuar.
+  pause
+  exit /b 0
 )
 
 :: 3) Garante TLS 1.2 para download via PowerShell
@@ -38,47 +41,8 @@ echo Instalando Node.js LTS...
 choco install nodejs-lts -y
 call "%AllUsersProfile%\chocolatey\bin\refreshenv.cmd"
 
-:CONTINUAR
-
-echo npm versão:
-npm -v
-
 echo.
 echo Node.js instalado/verificado com sucesso!
-
-:: 6) Baixa e extrai o repositório do GitHub
-echo Baixando repositório do GitHub...
-cd /d C:\
-
-:: Remove pasta existente se houver
-if exist "acessar" (
-  echo Removendo pasta existente...
-  rmdir /s /q "acessar"
-)
-
-:: Baixa o repositório usando git clone
-echo Clonando repositório...
-git clone https://github.com/ctp-maker/Expoe.git temp_repo
-
-:: Move a pasta específica para C:\acessar
-echo Movendo pasta para C:\acessar...
-move "temp_repo\acessos\acessar" "C:\acessar"
-
-:: Remove pasta temporária
-echo Limpando arquivos temporários...
-rmdir /s /q "temp_repo"
-
-echo Repositório baixado e extraído em C:\acessar
-
-:: 7) Instala dependências do projeto
-echo Instalando dependências do projeto...
-cd /d C:\acessar
-npm install
-
-echo Dependências instaladas com sucesso!
-
-node acessar/server.js
-
-echo Servidor iniciado com sucesso!
-
+echo.
+echo Agora execute o script instalar_projeto.cmd para continuar.
 pause
